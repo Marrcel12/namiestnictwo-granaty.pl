00ab7f9294614e567d6f330fe57fa4a6e62bc3f3
Marcel <marcel.kaminski@ucdconnect.ie>
2023-10-29 21:34:11 +0000

    adding teams

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(".option-img").hover(
     function (e) {
         $("." + e.target.className.split(" ")[1]).addClass('grey');
-        $("." + e.target.className.split(" ")[1]).children('button').addClass('show')
+        $("." + e.target.className.split(" ")[1]).children("a").children('button').addClass('show')
     }, // over
     function (e) {
         $("." + e.target.className.split(" ")[1]).removeClass('grey');
-        $("." + e.target.className.split(" ")[1]).children('button').removeClass('show')
+        $("." + e.target.className.split(" ")[1]).children("a").children('button').removeClass('show')
 
     } // out
 );
+
+$('.flipcard').click(function (e) {
+    var $card = $(this);
+    if ($card.hasClass("flipped")) {
+        $card.removeClass('flipped');
+    } else {
+        $card.addClass('flipped');
+    }
+});
 
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
